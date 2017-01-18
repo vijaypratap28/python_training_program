ebdf9318cbbc884d8b089dd19a477aafc73dac46
vijaypratap28 <vijaypratapsingh069@gmail.com>
2017-01-18 10:37:29 +0530

    Add files via upload

---
+++ day6_1.py
@@ -0,0 +1,24 @@
+class shape:
+	def __init__(self):
+		print 'shape'
+
+
+class rect(shape):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def area(self):
+        return self.x * self.y
+    def perimeter(self):
+        return 2 * self.x + 2 * self.y
+
+
+
+
+#rectangle = rect(10,20)
+
+
+#print  'area of rectangle is:', rectangle.area()
+#print  'perimeter of rectangle is:',rectangle.perimeter()
+	
